206700f300242f00018064c77fa354358c78a397
Ismail mayito <ismayito1@gmail.com>
2023-11-28 13:43:18 +0300

    Add Item and music class

--- classes/item.rb
+++ classes/item.rb
@@ -4,11 +4,23 @@ class Item
   attr_accessor :id, :genre, :source, :label
   attr_reader :archived
 
-  def initialize(id, genre, source, label, archived)
-    @id = id
+  def initialize(genre, source, archived, label, publish_date)
+    @id = Random.rand(1..1000)
     @genre = genre
     @source = source
     @label = label
     @archived = archived
+    @publish_date = publish_date
+  end
+
+  # a method that returns true if published date is older than 10 years
+  def can_be_archived?
+    current_date = Date.today
+    time_to_be_archived = current_date.year - @publish_date.year
+    time_to_be_archived > 10
+  end
+
+  def move_to_archive
+    @rchived = can_be_archived?
   end
 end
---
+++ classes/music.rb
@@ -0,0 +1,13 @@
+require './item'
+class Music < Item
+  def initialize(on_spotify)
+    super(genre, source, label, publish_date)
+    @on_spotify = on_spotify
+  end
+
+  # a method to override the default can_be_archived mrthod inthe parent class
+  # and return true if the can_be_archived is true and on_spotify is true else false
+  def can_be_archived?
+    super.can_be_archived? && @on_spotify
+  end
+end
