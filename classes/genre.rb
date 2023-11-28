1a8b16f128cdde2315fbe5a67c21ef724e046531
Ayokunnumi1 <omololuayk@gmail.com>
2023-11-28 05:43:57 +0100

    Fix linter errors

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,15 +1,15 @@
 class Genre
-    attr_accessor :name, :items
-    attr_reader :id
+  attr_accessor :name, :items
+  attr_reader :id
 
-    def initialize(name)
-        @id = rand(1..1000)
-        @name = name
-        @items = []
-    end
+  def initialize(name)
+    @id = rand(1..1000)
+    @name = name
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.genre = self
-    end
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 def print_options
-  puts "Options:"
-  puts "1. List of games"
-  puts "2. List all genres"
-  puts "3. List all labels"
-  puts "4. Add a game"
-  puts "5. Quit"
+  puts 'Options:'
+  puts '1. List of games'
+  puts '2. List all genres'
+  puts '3. List all labels'
+  puts '4. Add a game'
+  puts '5. Quit'
 end
