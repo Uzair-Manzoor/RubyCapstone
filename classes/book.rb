04e4b5de9a8fdd9ae829d95735ea4949a84ad785
Ayokunnumi1 <omololuayk@gmail.com>
2023-11-30 19:00:16 +0100

    Include getters & setters

--- classes/book.rb
+++ classes/book.rb
@@ -1,10 +1,12 @@
 require_relative 'item'
 
 class Book < Item
-    attr_accessor :publisher, :cover_state
+    attr_accessor :publisher, :cover_state, :publish_date
+    attr_reader :id
 
-    def initialize(publisher, cover_state)
+    def initialize(publisher, cover_state, archived, publish_date)
     super(archived, publish_date)
+    @id = rand(1..1000)
     @publisher = publisher
     @cover_state = cover_state
     end
