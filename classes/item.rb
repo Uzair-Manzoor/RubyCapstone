0513b457477f8d787209e4e8bfe803cf85f4f9fb
Ayokunnumi1 <omololuayk@gmail.com>
2023-11-30 02:06:21 +0100

    Edit item class at initialize and methods

--- classes/item.rb
+++ classes/item.rb
@@ -1,26 +1,30 @@
 require 'date' # Require the Date class
 
 class Item
-  attr_accessor :id, :genre, :source, :label
-  attr_reader :archived
+  attr_accessor  :archived, :label, :author, :source, :genre, :publish_date
+  attr_reader :id
 
-  def initialize(genre, source, archived, label, publish_date)
+  def initialize(archived:false, publish_date: nil)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @source = source
-    @label = label
     @archived = archived
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date) if publish_date
   end
 
   # a method that returns true if published date is older than 10 years
   def can_be_archived?
+    return false unless publish_date
     current_date = Date.today
     time_to_be_archived = current_date.year - @publish_date.year
     time_to_be_archived > 10
   end
 
   def move_to_archive
-    @rchived = can_be_archived?
+    @archived = can_be_archived?
   end
 end
+
+item1 = Item.new(archived: true, publish_date: '2022-01-13')
+puts 'code executed successfully'
+item1.move_to_archive
+puts 'false, it is less than 10'
+# item1 = Item.new(true, '2022-01-13')
