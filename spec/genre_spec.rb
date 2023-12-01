924800001741a81dd89f100521f3ac840e7da8bf
Ismail mayito <ismayito1@gmail.com>
2023-12-01 16:52:14 +0300

    fix linter errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -2,12 +2,12 @@ require_relative '../classes/genre'
 require_relative '../classes/item'
 
 describe Genre do
-  let(:item) { Item.new(1,'name' ,'2023-12-04') } # Replace Item with the actual class of items
+  let(:item) { Item.new(1, 'name', '2023-12-04') } # Replace Item with the actual class of items
 
   describe '#add_item' do
     it 'should add a new item to the items array' do
-      genre = Genre.new(1,'name')
+      genre = Genre.new(1, 'name')
       expect { genre.add_item(item) }.to change { genre.items.size }.by(1)
     end
-   end
+  end
 end
